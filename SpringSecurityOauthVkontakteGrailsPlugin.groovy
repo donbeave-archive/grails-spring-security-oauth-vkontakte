a763d6fa8fc5da69333a5846b2af0adee56bffc4
BeaVe <donbeave@gmail.com>
2013-11-01 22:37:58 +0800

    Initial commit.

---
+++ SpringSecurityOauthVkontakteGrailsPlugin.groovy
@@ -0,0 +1,28 @@
+/**
+* @author <a href='mailto:donbeave@gmail.com'>Alexey Zhokhov</a>
+*/
+class SpringSecurityOauthVkontakteGrailsPlugin {
+
+    def version = "0.1"
+    def grailsVersion = "1.2.2 > *"
+    def dependsOn = [springSecurityOauth: '2.0.2 > *']
+    def pluginExcludes = [
+        "grails-app/views/error.gsp"
+    ]
+
+    def title = "VK for Spring Security OAuth plugin"
+    def author = "Alexey Zhokhov"
+    def authorEmail = "donbeave@gmail.com"
+    def description = '''\
+Integrate [VK|http://www.vk.com] to [Spring Security plugin|http://grails.org/plugin/spring-security-oauth].
+'''
+
+    def documentation = "https://github.com/donbeave/grails-spring-security-oauth-vkontakte"
+
+    def license = "APACHE"
+
+    def developers = [[name: "Alexey Zhokhov", email: "donbeave@gmail.com"]]
+
+    def scm = [url: 'https://github.com/donbeave/grails-spring-security-oauth-vkontakte']
+
+}
