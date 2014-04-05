c3312c2c98f27e90d2af47c2e906053aa1f545b3
BeaVe <donbeave@gmail.com>
2014-04-05 18:42:04 +0800

    Added default config.

--- SpringSecurityOauthVkontakteGrailsPlugin.groovy
+++ SpringSecurityOauthVkontakteGrailsPlugin.groovy
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2012 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import grails.util.Environment
+
 /**
  * @author <a href='mailto:donbeave@gmail.com'>Alexey Zhokhov</a>
  */
@@ -24,5 +41,22 @@ Integrate [VK|http://www.vk.com] to [Spring Security OAuth plugin|http://grails.
     def scm = [url: 'https://github.com/donbeave/grails-spring-security-oauth-vkontakte']
 
     def loadAfter = ['spring-security-oauth']
+
+    def doWithSpring = {
+        loadConfig(application.config)
+    }
+
+    private void loadConfig(ConfigObject config) {
+        def classLoader = new GroovyClassLoader(getClass().classLoader)
+
+        // Note here the order of objects when calling merge - merge OVERWRITES values in the target object
+        // Load default config as a basis
+        def newConfig = new ConfigSlurper(Environment.current.name).parse(
+                classLoader.loadClass('DefaultVkontakteOauthConfig')
+        )
+
+        // Now merge DefaultVkontakteOauthConfig into the main config
+        config.merge(newConfig)
+    }
 
 }
